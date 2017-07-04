0f35b7c95563d1f6a5f9ebd74708358d6fc08d7d
Miriam Peralta <miiriampc@gmail.com>
2017-07-04 14:55:35 -0500

    update getJson

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -14,8 +14,8 @@ const state = {
 $( _ => {
   const url='https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AaM1cX4ewT6F7X49kuLFgcKCWUAgFM4mt0PH-4dEIuuRXKAoAwAAAAA&fields=id%2Clink%2Cnote%2Cboard%2Cimage%2Cmetadata%2Coriginal_link%2Ccreator';
     $.getJSON(url,(json) => {
-        state.user = json;
-        console.log(state.user);
+        state.pin = json;
+        console.log(state.pin);
         const root = $('#root');
         render(root);
     });
