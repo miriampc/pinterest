43d8c4771161bb9830efd270a47e631db9a8fb9b
Miriam Peralta <miiriampc@gmail.com>
2017-07-04 18:53:56 -0500

    modules 6to5 and uglify

---
+++ src/assets/js/components/board.js
@@ -0,0 +1,25 @@
+console.log("board");
+setTimeout(board(),1000);
+function board(){
+  console.log("boarddddd");
+}
+// const Board = () => {
+//   alert("hhh");
+  // state.pin.forEach((element) => {
+  //   console.log(element.image.original.url);
+  // });
+  //
+  // const boardContainer = $('<section class="board"></section>');
+  // const pinImage = $(`<div class="board__image col-md-2">
+  //                         <img src="https://s-media-cache-ak0.pinimg.com/originals/5b/98/19/5b9819400e331e8c5d9eef053a97250f.jpg" alt="Arabela Rojas">
+  //                     </div>`);
+  // const pintitle = $(`<div class="board__title"></div>`);
+  // const pinSubTitle = $(`<div class="board__subtitle"></div>`);
+  // const pinAuthor = $(`<div class="board__author">
+  //                         <img src="assets/images/arabelyuska.jpg" alt="Arabela Rojas">
+  //                         <h3></h3>
+  //                         <span>hola</span>
+  //                      </div>`);
+  // boardContainer.append(pinImage);
+  // return boardContainer;
+// }
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,20 +1,24 @@
 "use strict";
 const render = (root)=>{
     root.empty();
-    const section = $('<section class="components"></section>');
-
-    //section.append(Board(_=>render(root)));
+    const section = $('<section class="components">board</section>');
+    console.log(state.pin);
+    board();
+    // section.append(Board());
 
     root.append(section);
 };
 const state = {
     pin : null
-}
+};
+
 $( _ => {
   const url='https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AaM1cX4ewT6F7X49kuLFgcKCWUAgFM4mt0PH-4dEIuuRXKAoAwAAAAA&fields=id%2Clink%2Cnote%2Cboard%2Cimage%2Cmetadata%2Coriginal_link%2Ccreator';
-    $.getJSON(url,(json) => {
-        state.pin = json;
-        console.log(state.pin);
+    $.get(url,(json) => {
+        if(!json){
+          return alert("error");
+        }
+        state.pin = json.data;
         const root = $('#root');
         render(root);
     });
