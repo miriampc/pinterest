d972ed4806b3e7e2c84ff27e869eaf4aea3de117
Miriam Peralta <miiriampc@gmail.com>
2017-07-04 14:52:25 -0500

    update getJson

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -11,10 +11,10 @@ const state = {
     pin : null
 }
 $( _ => {
-
-    $.get('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AaM1cX4ewT6F7X49kuLFgcKCWUAgFM4mt0PH-4dEIuuRXKAoAwAAAAA&fields=id%2Clink%2Cnote%2Cboard%2Cimage%2Cmetadata%2Coriginal_link%2Ccreator',(error, data) => {
-        state.user = data;
-        console.log(state.user);
+  const url='https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AaM1cX4ewT6F7X49kuLFgcKCWUAgFM4mt0PH-4dEIuuRXKAoAwAAAAA&fields=id%2Clink%2Cnote%2Cboard%2Cimage%2Cmetadata%2Coriginal_link%2Ccreator';
+    $.getJSON(url,(json) => {
+        state.pin = json;
+        console.log(state.pin);
         const root = $('#root');
         render(root);
     });
