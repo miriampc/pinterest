543a227719c16c8e1fbe26781af4850841c8fa5f
Ariana Cabana <acabana.0904@gmail.com>
2017-07-04 18:54:06 -0500

    style head

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -1,10 +1,14 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 const render = (root)=>{
     root.empty();
     const section = $('<section class="components"></section>');
 
     //section.append(Board(_=>render(root)));
+    if (state.header == null) {
+      console.log("hola header");
+      section.append(Header(_ => render(root)));
+    }
 
     root.append(section);
 };
@@ -16,9 +20,9 @@ const state = {
 $( _ => {
  const url='https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AaM1cX4ewT6F7X49kuLFgcKCWUAgFM4mt0PH-4dEIuuRXKAoAwAAAAA&fields=id%2Clink%2Cnote%2Cboard%2Cimage%2Cmetadata%2Coriginal_link%2Ccreator';
    $.getJSON(url,(json) => {
-       state.user = json;
-       console.log(state.user.data);
+       state.pin = json;
+       console.log(state.pin.data);
        const root = $('#root');
        render(root);
    });
-})
+});
--- public/assets/js/components/header.js
+++ public/assets/js/components/header.js
@@ -1,21 +1,29 @@
 // console.log("hola");
 'use strict';
-const render = (root) =>{
-  root.empty();
-  const wrapper = $('<div class="wrapper"></div>');
+const Header = (update) => {
+  const nav = $('<nav class="col-xs-12 col-lg-12"></nav>');
+  const divBrand = $('<div class="navbar-brand col-xs-1 col-lg-1"></div>');
+  const divSearch = $('<div class="search col-xs-8 col-lg-8">');
+  const divBtn = $('<div class="navbar-btn col-xs-3 col-lg-3"></div>')
 
-  if (state.header == true) {
-    console.log("hola header");
-    wrapper.append(Header(_ => render(root)));
-  }
+  const brand = $('<a href="index.html"><img class="brand" alt="Logo Pinterest" src="assets/images/pinterest-logo.png"></a>');
+  const btnSearch = $('<button type="submit" class="search__input bg-btn"></button>')
+  const input = $('<input type="text" class="search__input search__input--sizew" placeholder="Buscar">');
+  const btnSave = $('<button type="submit" class="btn btn-default">Guardado</button>');
+  const btnCategory = $('<button type="submit" class="btn btn-default">Categorias</button>');
+  const btnNotification = $('<button type="submit" class="btn btn-default">Notificaciones</button>');
+
+  $('.components').append(nav);
+  nav.append(divBrand);
+  nav.append(divSearch);
+  nav.append(divBtn);
+
+  divBrand.append(brand);
+  divSearch.append(btnSearch);
+  divSearch.append(input);
+  divBtn.append(btnSave);
+  divBtn.append(btnCategory);
+  divBtn.append(btnNotification);
+
+  return nav;
 }
-
-const state = {
-  header : true
-};
-
-
-$(_ =>{
-  const root = $('#root');
-  render(root);
-});
