a7fe40290598d584fbe9944a8a02da79a82e21ff
Ariana Cabana <acabana.0904@gmail.com>
2017-07-05 03:53:39 -0500

    align items header

--- public/assets/js/components/header.js
+++ public/assets/js/components/header.js
@@ -2,16 +2,18 @@
 'use strict';
 const Header = (update) => {
   const nav = $('<nav class="col-xs-12 col-lg-12"></nav>');
-  const divBrand = $('<div class="navbar-brand col-xs-1 col-lg-1"></div>');
-  const divSearch = $('<div class="search col-xs-8 col-lg-8">');
-  const divBtn = $('<div class="navbar-btn col-xs-3 col-lg-3"></div>')
-
+  const divBrand = $('<div class="navbar-brand col-xs-2 col-lg-1 flex"></div>');
   const brand = $('<a href="index.html"><img class="brand" alt="Logo Pinterest" src="assets/images/pinterest-logo.png"></a>');
-  const btnSearch = $('<button type="submit" class="search__input bg-btn"></button>')
-  const input = $('<input type="text" class="search__input search__input--sizew" placeholder="Buscar">');
-  const btnSave = $('<button type="submit" class="btn btn-default">Guardado</button>');
-  const btnCategory = $('<button type="submit" class="btn btn-default">Categorias</button>');
-  const btnNotification = $('<button type="submit" class="btn btn-default">Notificaciones</button>');
+
+  const divSearch = $('<div class="search col-xs-4 col-lg-8  flex">');
+  const btnSearch = $('<button id="l" type="submit" class="bg-btn search__input"></button>')
+  const input = $('<input type="text" class="search__input search__input--sizew hidden-xs" placeholder="Buscar">');
+
+  const divBtn = $('<div class="navbar-btn col-xs-6 col-lg-3 flex"></div>')
+  const btnSave = $('<div class="flex__item"><button id="save" type="submit" class="bg-btn"></button></div>');
+  const btnCategory = $('<div class="flex__item"><button id="category" type="submit" class="bg-btn"></button></div>');
+  const btnNotification = $('<div class="flex__item"><button id="notify" type="submit" class="bg-btn"></button></div>');
+
 
   $('.components').append(nav);
   nav.append(divBrand);
