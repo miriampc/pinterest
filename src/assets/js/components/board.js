ccf0ec560747e056bb73e5bec192e0ceee3f9258
Miriam Peralta <miiriampc@gmail.com>
2017-07-05 19:59:49 -0500

    add icon search

--- src/assets/js/components/board.js
+++ src/assets/js/components/board.js
@@ -1,13 +1,19 @@
 const Board = () => {
   const boardContainer = $('<section class="board"></section>');
+  const addPin = $(`<div class="pin__newPin">
+                        <div><a href="" class="fa fa-plus"></a></div>
+                        <h2>Guardar Pin</h2>
+                    </div>`);
+
+  boardContainer.append(addPin);                  
   state.pin.forEach((element) => {
     const pinContainer = $('<div class="pin"></div>');
     const pinImage = $(`<div class="pin__image">
                           <img src="${element.image.original.url}" alt="Arabela Rojas">
                         </div>`);
       const pinAuthor = $(`<div class="pin__author">
-                              <div class="col-md-2"><img src="assets/images/arabelyuska.jpg" alt="Arabela Rojas"></div>
-                              <div class="col-md-8">
+                              <div class="col-xs-2 col-sm-2 col-md-2"><img src="assets/images/arabelyuska.jpg" alt="Arabela Rojas"></div>
+                              <div class="col-xs-8 col-sm-8 col-md-8">
                                 <h3>${element.creator.first_name}</h3>
                                 <span>${element.board.name}</span>
                               </div>
